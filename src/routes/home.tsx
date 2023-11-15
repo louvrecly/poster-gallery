1e663a939b3e55da8344e08499c71201255ee2ac
Louvre Chow <louvrecly@gmail.com>
2023-11-15 16:44:08 -0500

    reuse useGenres and useMovieData hooks in Home page

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,41 +1,32 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import MovieGrid from '../components/MovieGrid';
 import PageControl from '../components/PageControl';
-import { discoverMovies, getMovieGenres } from '../api/movies';
-import { MovieData, parseMovieData } from '../types/movie';
-import Genre, { createGenreMap } from '../types/genre';
+import useGenres from '../hooks/useGenres';
+import useMovieData from '../hooks/useMovieData';
+import { parseMovieData } from '../types/movie';
 
 const Home = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [pageCount, setPageCount] = useState(1);
-  const [movieItems, setMovieItems] = useState<MovieData[]>([]);
-  const [genres, setGenres] = useState<Genre[]>([]);
+  const { genreMap, isLoadingGenres } = useGenres();
+  const {
+    movieData,
+    pageCount,
+    currentPage,
+    isLoadingMovieData,
+    setCurrentPage,
+  } = useMovieData();
 
-  const genreMap = useMemo(() => createGenreMap(genres), [genres]);
+  const isLoading = useMemo(
+    () => isLoadingGenres || isLoadingMovieData,
+    [isLoadingGenres, isLoadingMovieData],
+  );
 
   const movies = useMemo(() => {
-    if (!movieItems.length || !Object.keys(genreMap).length) return [];
+    if (!movieData.length || !Object.keys(genreMap).length) return [];
 
-    return movieItems.map(parseMovieData(genreMap));
-  }, [genreMap, movieItems]);
-
-  useEffect(() => {
-    setIsLoading(true);
-    discoverMovies(-1, currentPage).then((res) => {
-      setPageCount(res.total_pages);
-      setMovieItems(res.results);
-      setIsLoading(false);
-    });
-  }, [currentPage]);
-
-  useEffect(() => {
-    getMovieGenres().then((res) => {
-      setGenres(res.genres);
-    });
-  }, []);
+    return movieData.map(parseMovieData(genreMap));
+  }, [genreMap, movieData]);
 
   return (
     <>
