82d4bf2efb90dfbd213541bdd02d91cfbfc25ff4
Louvre Chow <louvrecly@gmail.com>
2023-11-15 14:22:27 -0500

    create Movie type and parseMovieData function

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,3 +1,5 @@
+import Genre, { GenreMap } from './genre';
+
 export type MovieData = {
   id: number;
   title: string;
@@ -14,3 +16,40 @@ export type MovieData = {
   vote_average: number;
   vote_count: number;
 };
+
+type Movie = {
+  id: number;
+  title: string;
+  posterPath: string;
+  backdropPath: string;
+  releaseDate: Date;
+  originalLanguage: string;
+  originalTitle: string;
+  overview: string;
+  video: boolean;
+  adult: boolean;
+  genres: Genre[];
+  popularity: number;
+  voteAverage: number;
+  voteCount: number;
+};
+
+export function parseMovieData(genreMap: GenreMap) {
+  return (movieData: MovieData): Movie => {
+    const { genre_ids, ...rest } = movieData;
+
+    return {
+      ...rest,
+      posterPath: rest.poster_path,
+      backdropPath: rest.backdrop_path,
+      releaseDate: new Date(rest.release_date),
+      originalLanguage: rest.original_language,
+      originalTitle: rest.original_title,
+      voteAverage: rest.vote_average,
+      voteCount: rest.vote_count,
+      genres: genre_ids.map((id) => genreMap[id]),
+    };
+  };
+}
+
+export default Movie;
