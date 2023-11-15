3c698b43be1da4890c4a3a6c79729f0e86198831
Louvre Chow <louvrecly@gmail.com>
2023-11-15 10:36:40 -0500

    initialize data fetching in the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,46 @@
+import { useEffect, useState } from 'react';
+import { discoverMovies } from './api/movies';
+
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageCount, setPageCount] = useState(1);
+  const [movies, setMovies] = useState<Record<string, unknown>[]>([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    discoverMovies(currentPage).then((res) => {
+      const { results, total_pages } = res;
+
+      setPageCount(total_pages);
+      setMovies(results);
+      setIsLoading(false);
+    });
+  }, [currentPage]);
+
   return (
     <>
-      <h1>Hello World!</h1>
+      <h1>Poster Gallery</h1>
+
+      <div>{isLoading ? 'Loading' : JSON.stringify(movies)}</div>
+
+      <div>
+        <button
+          onClick={() => currentPage > 1 && setCurrentPage((page) => page - 1)}
+          disabled={currentPage <= 1}
+        >
+          Previous
+        </button>
+        {currentPage} / {pageCount}
+        <button
+          onClick={() =>
+            currentPage < pageCount && setCurrentPage((page) => page + 1)
+          }
+          disabled={currentPage >= pageCount}
+        >
+          next
+        </button>
+      </div>
     </>
   );
 }
