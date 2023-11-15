6baf87d348ac876c9b10ef9b0a31547c3717a3b5
Louvre Chow <louvrecly@gmail.com>
2023-11-15 13:18:38 -0500

    import MovieGrid to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { useEffect, useState } from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 import NavBar from './components/NavBar';
+import MovieGrid from './components/MovieGrid';
 import PageControl from './components/PageControl';
 import { discoverMovies } from './api/movies';
+import { MovieData } from './types/movie';
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [pageCount, setPageCount] = useState(1);
-  const [movies, setMovies] = useState<Record<string, unknown>[]>([]);
+  const [movies, setMovies] = useState<MovieData[]>([]);
 
   useEffect(() => {
     setIsLoading(true);
@@ -27,7 +31,23 @@ function App() {
 
       <NavBar />
 
-      <div>{isLoading ? 'Loading' : JSON.stringify(movies)}</div>
+      <Container
+        sx={{
+          py: 2,
+          flexGrow: 1,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+        }}
+      >
+        {isLoading ? (
+          <Typography variant="subtitle1" align="center">
+            Loading...
+          </Typography>
+        ) : (
+          <MovieGrid movies={movies} />
+        )}
+      </Container>
 
       <PageControl
         currentPage={currentPage}
