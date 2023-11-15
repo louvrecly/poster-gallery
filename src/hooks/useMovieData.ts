f153bffc9c9178bcd6493916eb304a5b3908360e
Louvre Chow <louvrecly@gmail.com>
2023-11-15 16:43:42 -0500

    create useMovieData hook

---
+++ src/hooks/useMovieData.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import { MovieData } from '../types/movie';
+import { discoverMovies } from '../api/movies';
+
+const useMovieData = (genreId: number = -1) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageCount, setPageCount] = useState(1);
+  const [movieData, setMovieItems] = useState<MovieData[]>([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    discoverMovies(genreId, currentPage).then((res) => {
+      setPageCount(res.total_pages);
+      setMovieItems(res.results);
+      setIsLoading(false);
+    });
+  }, [currentPage, genreId]);
+
+  return {
+    movieData,
+    pageCount,
+    currentPage,
+    isLoadingMovieData: isLoading,
+    setCurrentPage,
+  };
+};
+
+export default useMovieData;
