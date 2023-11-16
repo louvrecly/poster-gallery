c616bf18e573433d7f4f1fac157d74304d622f3a
Louvre Chow <louvrecly@gmail.com>
2023-11-15 21:20:38 -0500

    create searchMoviesByKeyword function

--- src/api/movies.ts
+++ src/api/movies.ts
@@ -4,14 +4,14 @@ import { MovieData } from '../types/movie';
 const TMDB_API_URL = import.meta.env.VITE_TMDB_API_URL;
 const TMDB_API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
-type DiscoverResponse = {
+type MovieListResponse = {
   page: number;
   total_pages: number;
   total_results: number;
   results: MovieData[];
 };
 
-const emptyDiscoverResponse: DiscoverResponse = {
+const emptyMovieListResponse: MovieListResponse = {
   page: 1,
   total_pages: 1,
   total_results: 0,
@@ -22,17 +22,33 @@ export async function discoverMovies(
   keyword: string = '',
   genreId: number = -1,
   page: number = 1,
-): Promise<DiscoverResponse> {
+): Promise<MovieListResponse> {
   try {
     const res = await fetch(
-      `${TMDB_API_URL}/3/discover/movie?sort_by=popularity.desc&api_key=${TMDB_API_KEY}&with_keywords=${keyword}&page=${page}${
+      `${TMDB_API_URL}/3/discover/movie?api_key=${TMDB_API_KEY}&with_keywords=${keyword}&page=${page}${
         genreId > 0 ? `&with_genres=${genreId}` : ''
-      }`,
+      }&sort_by=popularity.desc`,
     );
     return res.json();
   } catch (err) {
     console.log({ err });
-    return emptyDiscoverResponse;
+    return emptyMovieListResponse;
+  }
+}
+
+export async function searchMovieByKeyword(
+  keyword: string = '',
+  page: number = 1,
+): Promise<MovieListResponse> {
+  try {
+    const res = await fetch(
+      `${TMDB_API_URL}/3/search/movie?api_key=${TMDB_API_KEY}&query=${keyword}&include_adult=false&language=en-US&page=${page}`,
+    );
+
+    return res.json();
+  } catch (err) {
+    console.log({ err });
+    return emptyMovieListResponse;
   }
 }
 
