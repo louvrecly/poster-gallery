60612533672a589d12a0733ee897b9875b608dd7
Louvre Chow <louvrecly@gmail.com>
2023-11-17 16:00:29 -0500

    create InfoBlock using RatingCircle

---
+++ src/components/MovieGrid/MovieCard/InfoBlock.tsx
@@ -0,0 +1,35 @@
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import RatingCircle from '../../RatingCircle';
+
+interface InfoBlockProps {
+  voteAverage: number;
+  voteCount: number;
+  releaseDate: Date;
+  originalLanguage: string;
+}
+
+const InfoBlock = ({
+  voteAverage,
+  voteCount,
+  releaseDate,
+  originalLanguage,
+}: InfoBlockProps) => {
+  return (
+    <Stack direction="row" alignItems="center" spacing={2}>
+      <RatingCircle value={voteAverage} maxValue={10} count={voteCount} />
+
+      <Stack spacing={1}>
+        <Typography variant="body2">
+          Released: {releaseDate.toLocaleDateString()}
+        </Typography>
+
+        <Typography variant="body2">
+          Original Language: {originalLanguage.toUpperCase()}
+        </Typography>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default InfoBlock;
