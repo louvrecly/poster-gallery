3c58928ba48dacc52d898593c214e007e6ca75e7
Louvre Chow <louvrecly@gmail.com>
2023-11-21 14:42:52 -0500

    remove genre-page route

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App.tsx';
 import Home from './routes/home.tsx';
-import GenrePage from './routes/genre-page.tsx';
 import NotFound from './routes/not-found.tsx';
 import './index.css';
 
@@ -13,8 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
-
-          <Route path="genre/:genreId" element={<GenrePage />} />
 
           <Route path="*" element={<NotFound />} />
         </Route>
