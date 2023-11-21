34cf922e3140fcccb320d8511390c44c9763374c
Louvre Chow <louvrecly@gmail.com>
2023-11-21 14:48:03 -0500

    extract contents of MovieListView to Home page

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,18 +1,43 @@
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import GenreControl from '../components/GenreControl';
+import MovieGrid from '../components/MovieGrid';
+import PageControl from '../components/PageControl';
 import useSearchMovies from '../hooks/useSearchMovies';
-import MovieListView from '../components/MovieListView';
 
 const Home = () => {
   const { movies, pageCount, currentPage, isLoading, setCurrentPage } =
     useSearchMovies();
 
   return (
-    <MovieListView
-      movies={movies}
-      isLoading={isLoading}
-      currentPage={currentPage}
-      pageCount={pageCount}
-      navigateToPage={setCurrentPage}
-    />
+    <>
+      <GenreControl />
+
+      <Container
+        disableGutters
+        sx={{
+          py: 2,
+          flexGrow: 1,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+        }}
+      >
+        {isLoading ? (
+          <Typography variant="h6" align="center">
+            Loading...
+          </Typography>
+        ) : (
+          <MovieGrid movies={movies} />
+        )}
+      </Container>
+
+      <PageControl
+        currentPage={currentPage}
+        pageCount={pageCount}
+        navigateToPage={setCurrentPage}
+      />
+    </>
   );
 };
 
